e5fa88aca8b84f8422d7f2893694293a9cb7e4d4
David <clcdavi@gmail.com>
2019-11-10 18:37:55 -0300

    modificaciones en barra de navegacion

--- vistas/common/base.php
+++ vistas/common/base.php
@@ -15,34 +15,176 @@
     
     <link href="<?= PATH_CSS ?>/styles.css" rel="stylesheet">
 
+    <!-- Font Awesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+	<!-- Bootstrap core CSS -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Material Design Bootstrap -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.10/css/mdb.min.css" rel="stylesheet">
+
 	<!-- favIcon -->
 	<link href="<?=PATH_IMAGENES?>/adaraNuevoLogo.ico" rel="icon">
 
 	<!-- fuente -->
-
+	
+	<style>
+		.gris{
+			background: #D75093;
+		}
+		.top-nav-collapse{
+			background: rgba(0,0,0,0.350);
+			
+		}
+		.adaraNombre{
+			font-size: 2rem;
+		}
+		/* .navbar{
+			height:70px;
+		} */
+	</style>
 </head>
 <body>
 	<header>
-		<div class="container-fluid">
+
+		<nav class="navbar navbar-light bg-light">
+			<a class="navbar-brand" href="#">
+				<img src="<?=PATH_IMAGENES?>/adaraNuevoLogo.ico" width="30" height="30" alt="">
+			</a>
+		</nav>
+
+		<!-- <nav class="navbar navbar-expand-lg navbar-dark gris scrolling-navbar fixed-top navLogoPequeño">
+
+			<div class="container">
+				<a class="navbar-brand adaraNombre" href="index.php?m=index"><img id="logoPequeño" src="<?=PATH_IMAGENES?>/adaraNuevoLogo.ico" alt="logoPequeño"></a>
+				
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						
+				<span class="navbar-toggler-icon"></span>
+						
+				</button>
+				
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+				    <ul class="navbar-nav mr-auto">
+							    	
+				    	<li class="nav-item active">
+								        
+					    	<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+								    	
+						</li>
+								    	
+						<li class="nav-item">
+								        
+							<a class="nav-link" href="#">Link</a>
+						
+						</li>
+								      
+						<li class="nav-item">
+								        
+							<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+								    	
+						</li>
+					</ul>
+
+					<ul class="navbar-nav nav-flex-icons">
+						
+						<li class="nav-item">
+							
+							<a href="https://www.facebook.com/ADARA-1605857726169572/" class="nav-link">
+								
+								<i class="fab fa-facebook-f fa-fw blue-text"></i>
+
+							</a>
+
+						</li>
+
+						<li class="nav-item">
+							
+							<a href="https://www.instagram.com/adara_nails_beauty/" class="nav-link">
+								
+								<i class="fab fa-instagram fa-fw pink-text bordercolor-pink"></i>
+
+							</a>
+
+						</li>
+
+						<li class="nav-item">
+							
+							<a href="#" class="nav-link">
+								
+								<i class="fab fa-twitter fa-fw blue-text"></i>
+
+							</a>
+
+						</li>
+
+					</ul>
+
+				</div>
+
+			</div>
+
+		</nav> -->
+
 			<div class="row d-flex justify-content-center">
 
-				<div class="m-3">
+				<div class="mt-5 pt-4">
 					<img id="adaraNombre" src="<?=PATH_IMAGENES?>/adaraNombre.png" alt="adaraNombre">
 				</div>
 
 			</div>
 
-			<div class="row d-flex justify-content-center">
-				
-				<div style="height: 100px;width: 100px;" class="bg-indigo">
-					<div>
-						
-					</div>
-				</div>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
 
-			</div>
-
-		</div>
 	</header>
 	<nav>
 		
@@ -53,6 +195,25 @@
 	<footer>
 		
 	</footer>
+
+	<script>
+		(function(){
+		//timeout
+			var saludo = function() {
+				console.log('saludos!');
+			};
+			setTimeout(saludo,3000);
+		})
+	</script>
+
+	<!-- JQuery -->
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<!-- Bootstrap tooltips -->
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+	<!-- Bootstrap core JavaScript -->
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<!-- MDB core JavaScript -->
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.10/js/mdb.min.js"></script>
 
 	<script src="<?= PATH_VENDOR ?>/jquery/jquery-3.4.1.min.js"></script>
 	<script src="<?= PATH_VENDOR ?>/popper/popper.min.js"></script>
